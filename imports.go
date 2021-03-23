d9fcaec2b702abf6a70c393b8826e8926c635c0f
Charlie Vieth <charlie.vieth@gmail.com>
2021-03-23 19:56:18 -0400

    move Process to seperate file

--- imports.go
+++ imports.go
@@ -13,7 +13,6 @@ import (
 	"bytes"
 	"fmt"
 	"go/ast"
-	"go/build"
 	"go/format"
 	"go/parser"
 	"go/printer"
@@ -38,17 +37,6 @@ type Options struct {
 
 	FormatOnly  bool // Disable the insertion and deletion of imports
 	SimplifyAST bool // simplify code
-}
-
-// Process formats and adjusts imports for the provided file.
-// If opt is nil the defaults are used.
-//
-// Note that filename's directory influences which imports can be chosen,
-// so it is important that filename be accurate.
-// To process data ``as if'' it were in filename, pass the data as a non-nil src.
-func Process(filename string, src []byte, opt *Options) ([]byte, error) {
-	env := &fixEnv{GOPATH: build.Default.GOPATH, GOROOT: build.Default.GOROOT}
-	return process(filename, src, opt, env)
 }
 
 func process(filename string, src []byte, opt *Options, env *fixEnv) ([]byte, error) {
