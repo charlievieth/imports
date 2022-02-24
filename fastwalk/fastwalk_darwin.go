91f1444bde535a81e96ef9cd449eb4e125baf85b
Charlie Vieth <charlie.vieth@gmail.com>
2022-02-23 23:01:18 -0500

    fastwalk: improve darwin perf by ~3x
    ```
    name         old time/op    new time/op    delta
    FastWalk-10    48.4ms ± 3%    16.6ms ± 3%  -65.77%  (p=0.000 n=10+10)
    
    name         old alloc/op   new alloc/op   delta
    FastWalk-10    1.30MB ± 0%    1.29MB ± 0%   -0.48%  (p=0.000 n=10+10)
    
    name         old allocs/op  new allocs/op  delta
    FastWalk-10     28.2k ± 0%     25.5k ± 0%   -9.34%  (p=0.000 n=9+10)
    ```

--- fastwalk/fastwalk_darwin.go
+++ fastwalk/fastwalk_darwin.go
@@ -1,35 +1,96 @@
-// Copyright 2016 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-//go:build appengine || darwin
-// +build appengine darwin
+//go:build darwin
+// +build darwin
 
 package fastwalk
 
-import "os"
+import (
+	"os"
+	"syscall"
+	"unsafe"
+)
 
-// readDir calls fn for each directory entry in dirName.
-// It does not descend into directories or follow symlinks.
-// If fn returns a non-nil error, readDir returns with that error
-// immediately.
 func readDir(dirName string, fn func(dirName, entName string, typ os.FileMode) error) error {
-	fis, err := os.ReadDir(dirName)
+	fd, err := openDir(dirName)
 	if err != nil {
-		return err
+		return &os.PathError{Op: "opendir", Path: dirName, Err: err}
 	}
+	defer closedir(fd)
+
 	skipFiles := false
-	for _, fi := range fis {
-		if fi.Type().IsRegular() && skipFiles {
-			continue
-		}
-		if err := fn(dirName, fi.Name(), fi.Type()); err != nil {
-			if err == ErrSkipFiles {
-				skipFiles = true
+	var dirent syscall.Dirent
+	var entptr *syscall.Dirent
+	for {
+		if errno := readdir_r(uintptr(fd), &dirent, &entptr); errno != 0 {
+			if errno == syscall.EINTR {
 				continue
 			}
-			return err
+			return &os.PathError{Op: "readdir", Path: dirName, Err: errno}
+		}
+		if entptr == nil { // EOF
+			break
+		}
+		if dirent.Ino == 0 {
+			continue
+		}
+		typ := dtToType(dirent.Type)
+		if skipFiles && typ.IsRegular() {
+			continue
+		}
+		name := (*[len(syscall.Dirent{}.Name)]byte)(unsafe.Pointer(&dirent.Name))[:]
+		for i, c := range name {
+			if c == 0 {
+				name = name[:i]
+				break
+			}
+		}
+		// Check for useless names before allocating a string.
+		if string(name) == "." || string(name) == ".." {
+			continue
+		}
+		nm := string(name)
+		if err := fn(dirName, nm, typ); err != nil {
+			if err != ErrSkipFiles {
+				return err
+			}
+			skipFiles = true
 		}
 	}
+
 	return nil
+}
+
+func dtToType(typ uint8) os.FileMode {
+	switch typ {
+	case syscall.DT_BLK:
+		return os.ModeDevice
+	case syscall.DT_CHR:
+		return os.ModeDevice | os.ModeCharDevice
+	case syscall.DT_DIR:
+		return os.ModeDir
+	case syscall.DT_FIFO:
+		return os.ModeNamedPipe
+	case syscall.DT_LNK:
+		return os.ModeSymlink
+	case syscall.DT_REG:
+		return 0
+	case syscall.DT_SOCK:
+		return os.ModeSocket
+	}
+	return ^os.FileMode(0)
+}
+
+// According to https://golang.org/doc/go1.14#runtime
+// A consequence of the implementation of preemption is that on Unix systems,
+// including Linux and macOS systems, programs built with Go 1.14 will receive
+// more signals than programs built with earlier releases.
+//
+// This causes opendir to sometimes fail with EINTR errors.
+// We need to retry in this case.
+func openDir(path string) (fd uintptr, err error) {
+	for {
+		fd, err := opendir(path)
+		if err != syscall.EINTR {
+			return fd, err
+		}
+	}
 }
